084d9a55aa0c8c5b2406121eec14fc64b7f92992
Alexander <a.biryukov164@gmail.com>
2024-01-27 16:29:01 +0400

    revision of tests

--- tests/test_dicts.py
+++ tests/test_dicts.py
@@ -5,4 +5,4 @@ def test_get_val():
     assert get_val({"1": "elefant", "2": "fox", "3": "mouse"}, "1") == "elefant"
     assert get_val({"1": "elefant", "2": "fox", "3": "mouse"}, "4") == "unicorn"
     assert get_val({}, "1", "fish") == "fish"
-
+    assert get_val({"1": "elefant", "2": "fox", "3": "mouse"}, "1", "fish") == "elefant"
